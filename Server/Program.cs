d6c31d78ee99d39b83e8c95ecd77a5f17af9dd61
ast <atrusov@gmail.com>
2023-07-17 13:04:08 +0300

    blank db and bot token

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using System.Text.Json;
+
+var botClient = new TelegramBotClient("TOKEN");
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+var meJson = JsonSerializer.Serialize(me, new JsonSerializerOptions { WriteIndented = true });
+Console.WriteLine(meJson);
