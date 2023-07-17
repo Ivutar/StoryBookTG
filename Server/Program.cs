c51228d93933ed736ecf687bf241408af088b8b5
ast <atrusov@gmail.com>
2023-07-17 13:18:55 +0300

    start

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,38 @@
 ﻿using Telegram.Bot;
 using System.Text.Json;
+using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
 
-var botClient = new TelegramBotClient("TOKEN");
+const string TOKEN = "TOKEN";
+var opt = new JsonSerializerOptions { WriteIndented = true };
+using CancellationTokenSource cts = new ();
+
+var botClient = new TelegramBotClient(TOKEN);
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+ReceiverOptions receiverOptions = new ()
+{
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandleErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
 
 var me = await botClient.GetMeAsync();
-Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+Console.WriteLine(JsonSerializer.Serialize(me, opt));
+Console.ReadLine(); //wait to exit
 
-var meJson = JsonSerializer.Serialize(me, new JsonSerializerOptions { WriteIndented = true });
-Console.WriteLine(meJson);
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    Console.WriteLine(JsonSerializer.Serialize(update,opt));
+}
+
+async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    Console.WriteLine(JsonSerializer.Serialize(exception, opt));
+}
